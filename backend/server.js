415101010162f4bec38e19e564f3e18be2c25530
Nick Prosch <mail@nick-prosch.de>
2024-10-31 18:34:38 +0100

    28:20

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,25 @@ app.get('/', (req, res) => {
     res.send("Server is ready.")
 })
 
+app.post('/products', async (req, res) => {
+
+    const product = req.body
+
+    if (!product.name || !product.price || !product.image) { // if any of the fields are missing
+        return res.status(400).json({ success: false, message: "All fields are required." })
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save()
+        res.status(201).json({ success: true, message: "Product created.", data: newProduct })
+    } catch (error) {
+        console.error("Product not created: ", error.message)
+        res.status(500).json({ success: false, message: "Product not created.", error: error.message })
+    }
+})
+
 app.listen(3000, () => {
     connectDb()
     console.log("Server started.")
