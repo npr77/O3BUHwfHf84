89d4d69aa63a2a41816b9f7a762b6efd10a4f0d4
Nick Prosch <mail@nick-prosch.de>
2024-11-04 15:41:14 +0100

    54:24

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { connectDb } from './config/db.js'
-import Product from './models/product.model.js'
-import mongoose from 'mongoose'
+
+import productRoutes from './routes/product.route.js'
 
 const app = express()
 
@@ -11,63 +11,7 @@ app.get('/', (req, res) => {
 
 app.use(express.json())
 
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({})
-        res.status(200).json({ success: true, data: products })
-    } catch (error) {
-        console.error("Error fetching products: ", error.message)
-        res.status(500).json({ success: false, message: error.message })
-    }
-})
-
-app.post('/api/products', async (req, res) => {
-
-    const product = req.body
-
-    if (!product.name || !product.price || !product.image) { // if any of the fields are missing
-        return res.status(400).json({ success: false, message: "All fields are required." })
-    }
-
-    const newProduct = new Product(product)
-
-    try {
-        await newProduct.save()
-        res.status(201).json({ success: true, message: "Product created.", data: newProduct })
-    } catch (error) {
-        console.error("Product not created: ", error.message)
-        res.status(500).json({ success: false, message: "Product not created.", error: error.message })
-    }
-})
-
-app.put('/api/products/:id', async (req, res) => {
-    const { id } = req.params
-    const product = req.body
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ success: false, message: "Invalid Product Id." })
-    }
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true })
-        res.status(200).json({ success: true, message: "Product updated.", data: updatedProduct })
-    } catch (error) {
-        console.error("Product not updated: ", error.message)
-        res.status(500).json({ success: false, message: "Server Error" })
-    }
-})
-
-app.delete('/api/products/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        await Product.findByIdAndDelete(id)
-        res.status(200).json({ success: true, message: "Product deleted." })
-    } catch (error) {
-        console.error("Product not de leted: ", error.message)
-        res.status(404).json({ success: false, message: "Product not found." })
-    }
-
-})
+app.use('/api/products', productRoutes)
 
 app.listen(3000, () => {
     connectDb()
