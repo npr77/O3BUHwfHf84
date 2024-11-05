e69d41369ac56be3a73a6ca8beac1c9433b7bef2
Nick Prosch <mail@nick-prosch.de>
2024-11-05 15:16:47 +0100

    1:10:58 Product Store colour not working

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Container, Flex, HStack, Text } from '@chakra-ui/react'
+import { CiSquarePlus } from "react-icons/ci";
+
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <Container maxW={"1140px"} px={4}>
+            <Flex
+                h={16}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+                flexDir={{
+                    base: "column",
+                    sm: "row",
+                }}
+            >
+                <Text
+                    fontSize={{ base: "22", sm: "28" }}
+                    fontWeight={"bold"}
+                    textTransform={"uppercase"}
+                >
+                    <Link to={"/"}>Product Store 🛒</Link>
+                </Text>
+                <HStack spacing={2} alignItems={"center"}>
+                    <Link to={"/create"}>
+                        <Button>
+                            <CiSquarePlus />
+                        </Button>
+                    </Link>
+
+                </HStack>
+            </Flex>
+        </Container >
     )
 }
 
