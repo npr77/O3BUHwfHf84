4365207364d108caf61c241833c68e1c46d1b06e
Nick Prosch <mail@nick-prosch.de>
2024-11-06 09:54:40 +0100

    1:16:51

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,24 @@
+import { Box, Container, Heading, useColorModeValue, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { useState } from 'react'
 
 const CreatePage = () => {
+
+    const [newProduction, setNewProduction] = useState({
+        name: "",
+        price: "",
+        image: "",
+    })
+
     return (
-        <div>CreatePage</div>
+        <Container maxW={"container.sm"}>
+            <VStack spacing={8}>
+                <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>Create New Product</Heading>
+                <Box w={"100%"} bg={useColorModeValue("white", "gray.800")} p={8} rounded={"lg"} shadow={"md"}>
+
+                </Box>
+            </VStack>
+        </Container>
     )
 }
 
