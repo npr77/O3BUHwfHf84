ee84af9d4ecb3f1ec4b3e8dbad1f7e013417b146
Nick Prosch <mail@nick-prosch.de>
2024-11-07 15:00:01 +0100

    1:37:25

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Container, Text, textDecoration, VStack } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <Container maxWidth="container.xl" py={12}>
+            <VStack spacing={8}>
+                <Text
+                    fontSize={"30"}
+                    fontWeight={"bold"}
+                    bgGraient="linear(to-r, #000000, #ffffff)"
+                    bgClip={"text"}
+                    textAling={"center"}
+                >
+                    Current Products 🚀
+                </Text>
+
+                <Text fontSize={"xl"} textAling={"center"} fontWeight='bold' color="gray.500">
+                    No Products found 😞{" "}
+                    <Link to={"/create"}>
+                        <Text as={"span"} color={"blue.500"} _hover={{ textDecoration: "underline" }}>
+                            Create a product
+                        </Text>
+                    </Link>
+
+                </Text>
+
+            </VStack>
+        </Container>
     )
 }
 
